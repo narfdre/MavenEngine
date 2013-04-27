d5aaaae5aed00354e28ed15f9b877d4c00097b69
Andre Kradolfer <narfdre@gmail.com>
2013-04-27 17:23:28 -0600

    adding a service to test guice injecting

--- src/main/java/co/ohba/MavenEngine/resources/TestResource.java
+++ src/main/java/co/ohba/MavenEngine/resources/TestResource.java
@@ -1,10 +1,16 @@
 package co.ohba.MavenEngine.resources;
 
+import co.ohba.MavenEngine.services.TestService;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/test")
 public class TestResource {
+
+    @Inject
+    TestService testService;
 
     @GET
     public String getTest(){
@@ -15,5 +21,11 @@ public class TestResource {
     @Path("/another")
     public String getAnotherTest(){
         return "awesome sauce";
+    };
+
+    @GET
+    @Path("/service")
+    public String getServiceTest(){
+       return testService.helloWorld();
     };
 }
---
+++ src/main/java/co/ohba/MavenEngine/services/TestService.java
@@ -0,0 +1,8 @@
+package co.ohba.MavenEngine.services;
+
+public class TestService {
+
+    public String helloWorld(){
+        return "Hello World From Service";
+    }
+}
